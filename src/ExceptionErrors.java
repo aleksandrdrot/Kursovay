baee91e0ebcdc07b1610d9cc07547f73fcd30193
aleksandrdrot <aleksandr_drot@rambler.ru>
2023-02-03 18:55:35 +0300

    Курсовая готова

---
+++ src/ExceptionErrors.java
@@ -0,0 +1,11 @@
+public class ExceptionErrors {
+    public void error1(){
+        System.out.println("\nДанные введены некоректно!\n");
+    }
+    public void error2(){
+        System.out.println("\nСписок зополнен! Создайте новый список!\n");
+    }
+    public void error3(){
+        System.out.println("\nСовпадений не найдено!\n");
+    }
+}
